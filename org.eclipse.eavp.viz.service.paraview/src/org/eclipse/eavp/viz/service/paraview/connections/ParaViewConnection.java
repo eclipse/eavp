eb13b38e4f66ab657639528867e745c8811ddfdc
Robert Smith <smithrw@ornl.gov>
2016-04-21 11:36:16 -0400

    Improved ParaView connections
    ParaView connections now give more descriptive error messages and
    meaningful default paths on the Preferences page.
    
    Signed-off-by: Robert Smith <smithrw@ornl.gov>

--- org.eclipse.eavp.viz.service.paraview/src/org/eclipse/eavp/viz/service/paraview/connections/ParaViewConnection.java
+++ org.eclipse.eavp.viz.service.paraview/src/org/eclipse/eavp/viz/service/paraview/connections/ParaViewConnection.java
@@ -11,7 +11,13 @@
  *******************************************************************************/
 package org.eclipse.eavp.viz.service.paraview.connections;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
@@ -69,6 +75,7 @@ public class ParaViewConnection extends VizConnection<IParaViewWebClient> {
 		String host = getHost();
 		String port = Integer.toString(getPort());
 		String serverPath = getProperty("serverPath");
+		String visualizerPort = getProperty("visualizerPort");
 
 		// The system delimiter for directories
 		String delimiter = System.getProperty("file.separator");
@@ -81,6 +88,134 @@ public class ParaViewConnection extends VizConnection<IParaViewWebClient> {
 		// If the server path ends with the delimiter, remove it
 		if (serverPath.endsWith(delimiter)) {
 			serverPath = serverPath.substring(0, path.length() - 1);
+		}
+
+		try {
+
+			// Check the host name to see if this is a local launch
+			InetAddress hostAddr = InetAddress.getByName(host);
+			if (hostAddr.isAnyLocalAddress() || hostAddr.isLoopbackAddress()
+					|| NetworkInterface.getByInetAddress(hostAddr) != null) {
+
+				// Get the system name
+				String os = System.getProperty("os.name", "generic")
+						.toLowerCase(Locale.ENGLISH);
+
+				// Get the specified directory
+				File dir = new File(path);
+
+				// If the directory exists, search inside it
+				if (dir.exists()) {
+
+					// Check inside the directory, looking for ParaView. The
+					// ParaView
+					// directory will have different structures on different
+					// operation systems, requiring that different places be
+					// checked in each case. If the file cannot be found, give
+					// an error message
+					if ((os.indexOf("mac") >= 0)
+							|| (os.indexOf("darwin") >= 0)) {
+						if (!new File(
+								dir + "/paraview.app/Contents/bin/pvpython")
+										.exists()) {
+							addErrorMessage(
+									"Could not find ParaView in directory \""
+											+ dir + "\".");
+						}
+					} else if (os.indexOf("win") >= 0) {
+						// TODO Specify where pvpython is inside a Windows
+						// install
+					} else if (os.indexOf("nux") >= 0) {
+						if (!new File(dir + "/bin/pvpython").exists()) {
+							addErrorMessage(
+									"Could not find VisIt in directory \"" + dir
+											+ "\".");
+						}
+					}
+				} else {
+					// If the directory doesn't exist, give an error message
+					addErrorMessage(
+							"Could not find directory \"" + dir + "\".");
+				}
+
+				// Get the directory containing the server Python script
+				File serverDir = new File(serverPath);
+
+				// Check that the server directory exists
+				if (serverDir.exists()) {
+
+					// Check if the server script can be found and give an error
+					// message if not
+					if (!new File(serverDir + "/http_pvw_server.py").exists()) {
+						addErrorMessage(
+								"Could not find http_pvw_server.py in \""
+										+ serverPath + "\".");
+					}
+				} else {
+
+					// If the server directory does no exist, give an error
+					// message
+					addErrorMessage(
+							"Could not find directory \"" + serverDir + "\".");
+				}
+
+				// A test socket for checking the port
+				Socket s = null;
+				try {
+
+					// Try to open a port to the given number
+					s = new Socket("localhost", Integer.parseInt(port));
+
+					// If something responded, then the port is already in use.
+					addErrorMessage("Port number " + port + " is in use.");
+				} catch (NumberFormatException | IOException e) {
+					// We expect an IOException here if the port is not
+					// currently being used, so there is nothing to do
+				} finally {
+
+					// Close the socket, if one was made
+					if (s != null) {
+						try {
+							s.close();
+						} catch (IOException e) {
+							logger.error(
+									"Error while closing the test socket.");
+						}
+					}
+				}
+
+				try {
+
+					// Try to open a port to the visualizer's port number
+					s = new Socket("localhost",
+							Integer.parseInt(visualizerPort));
+
+					// If something responded, then the port is already in use.
+					addErrorMessage(
+							"Port number " + visualizerPort + " is in use.");
+				} catch (NumberFormatException | IOException e) {
+					// We expect an IOException here if the port is not
+					// currently being used, so there is nothing to do
+				} finally {
+
+					// Close the socket, if one was made
+					if (s != null) {
+						try {
+							s.close();
+						} catch (IOException e) {
+							logger.error(
+									"Error while closing the test socket.");
+						}
+					}
+				}
+
+			}
+		} catch (UnknownHostException | SocketException e) {
+
+			// If a problem occurred while trying to resolve the host name, warn
+			// the user
+			addErrorMessage(
+					"Could not find server with host name \"" + host + "\"");
 		}
 
 		// Run the process that will launch the http server in ParaView Python
