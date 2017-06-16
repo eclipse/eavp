bd646b5cdda4e757c4277166399b15769b6cc0f6
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-06-16 16:32:25 +0200

    Demo charts have been added.

---
+++ org.eclipse.eavp.service.swtchart.demos/src/org/eclipse/eavp/service/swtchart/demos/parts/LineSeries_2_Part.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eavp.service.swtchart.demos.parts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.eavp.service.swtchart.core.ColorAndFormatSupport;
+import org.eclipse.eavp.service.swtchart.core.IChartSettings;
+import org.eclipse.eavp.service.swtchart.core.IPrimaryAxisSettings;
+import org.eclipse.eavp.service.swtchart.core.ISeriesData;
+import org.eclipse.eavp.service.swtchart.demos.support.SeriesConverter;
+import org.eclipse.eavp.service.swtchart.linecharts.ILineSeriesData;
+import org.eclipse.eavp.service.swtchart.linecharts.ILineSeriesSettings;
+import org.eclipse.eavp.service.swtchart.linecharts.LineChart;
+import org.eclipse.eavp.service.swtchart.linecharts.LineSeriesData;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.swtchart.IAxis.Position;
+
+public class LineSeries_2_Part extends LineChart {
+
+	@Inject
+	public LineSeries_2_Part(Composite parent) {
+		super(parent, SWT.NONE);
+		setBackground(ColorAndFormatSupport.COLOR_WHITE);
+		try {
+			initialize();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private void initialize() throws Exception {
+
+		/*
+		 * Chart Settings
+		 */
+		IChartSettings chartSettings = getChartSettings();
+		chartSettings.setOrientation(SWT.HORIZONTAL);
+		chartSettings.setHorizontalSliderVisible(true);
+		chartSettings.setVerticalSliderVisible(false);
+		chartSettings.setUseZeroX(false);
+		chartSettings.setUseZeroY(false);
+		/*
+		 * Primary X-Axis
+		 */
+		IPrimaryAxisSettings primaryAxisSettingsX = chartSettings.getPrimaryAxisSettingsX();
+		primaryAxisSettingsX.setTitle("Time");
+		primaryAxisSettingsX.setDecimalFormat(ColorAndFormatSupport.decimalFormatVariable);
+		primaryAxisSettingsX.setColor(ColorAndFormatSupport.COLOR_BLACK);
+		primaryAxisSettingsX.setPosition(Position.Primary);
+		/*
+		 * Primary Y-Axis
+		 */
+		IPrimaryAxisSettings primaryAxisSettingsY = chartSettings.getPrimaryAxisSettingsY();
+		primaryAxisSettingsY.setTitle("Intensity");
+		primaryAxisSettingsY.setDecimalFormat(ColorAndFormatSupport.decimalFormatScientific);
+		primaryAxisSettingsY.setColor(ColorAndFormatSupport.COLOR_BLACK);
+		//
+		applySettings(chartSettings);
+		/*
+		 * Create series.
+		 */
+		List<ILineSeriesData> lineSeriesDataList = new ArrayList<ILineSeriesData>();
+		ISeriesData seriesData = SeriesConverter.getSeriesXY(SeriesConverter.LINE_SERIES_2);
+		//
+		ILineSeriesData lineSeriesData = new LineSeriesData(seriesData);
+		ILineSeriesSettings lineSerieSettings = lineSeriesData.getLineSeriesSettings();
+		lineSerieSettings.setEnableArea(false);
+		lineSeriesDataList.add(lineSeriesData);
+		/*
+		 * Set series.
+		 */
+		addSeriesData(lineSeriesDataList, false);
+	}
+}
