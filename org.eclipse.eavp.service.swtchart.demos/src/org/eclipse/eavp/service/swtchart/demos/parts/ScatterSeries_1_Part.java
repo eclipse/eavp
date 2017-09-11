a65f16892c63c429cf1a29a458d6387e0ba1ea20
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-09-11 14:11:07 +0200

    The scatter chart edit demo page has been added.

--- org.eclipse.eavp.service.swtchart.demos/src/org/eclipse/eavp/service/swtchart/demos/parts/ScatterSeries_1_Part.java
+++ org.eclipse.eavp.service.swtchart.demos/src/org/eclipse/eavp/service/swtchart/demos/parts/ScatterSeries_1_Part.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.eclipse.eavp.service.swtchart.core.BaseChart;
 import org.eclipse.eavp.service.swtchart.core.IChartSettings;
 import org.eclipse.eavp.service.swtchart.core.ISeriesData;
 import org.eclipse.eavp.service.swtchart.customcharts.PCAChart;
@@ -26,13 +25,9 @@ import org.eclipse.eavp.service.swtchart.scattercharts.IScatterSeriesSettings;
 import org.eclipse.eavp.service.swtchart.scattercharts.ScatterSeriesData;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
 import org.swtchart.ILineSeries.PlotSymbolType;
-import org.swtchart.ISeries;
 
 public class ScatterSeries_1_Part extends PCAChart {
 
@@ -43,10 +38,6 @@ public class ScatterSeries_1_Part extends PCAChart {
 	private Color COLOR_GRAY = Display.getDefault().getSystemColor(SWT.COLOR_GRAY);
 	//
 	private int SYMBOL_SIZE = 8;
-	//
-	private static final int KEY_CODE_R = 114;
-	private static final int KEY_CODE_S = 115;
-	private boolean isCustomSelection = false;
 
 	@Inject
 	public ScatterSeries_1_Part(Composite parent) {
@@ -84,80 +75,6 @@ public class ScatterSeries_1_Part extends PCAChart {
 		 * Set series.
 		 */
 		addSeriesData(scatterSeriesDataList);
-	}
-
-	@Override
-	public void handleKeyUpEvent(Event event) {
-
-		super.handleKeyUpEvent(event);
-		isCustomSelection = false;
-		//
-		if(event.keyCode == KEY_CODE_R) {
-			/*
-			 * Reset Selection
-			 */
-			System.out.println("Reset Selection");
-			BaseChart baseChart = getBaseChart();
-			baseChart.resetSeriesSettings();
-		} else if(event.keyCode == KEY_CODE_S) {
-			/*
-			 * Custom Selection
-			 */
-			System.out.println("Custom Selection Activated");
-			isCustomSelection = true;
-		}
-	}
-
-	@Override
-	public void handleMouseUpEvent(Event event) {
-
-		super.handleMouseUpEvent(event);
-		//
-		if(isCustomSelection) {
-			/*
-			 * Set Selection
-			 */
-			System.out.println("Set Selection");
-			//
-			BaseChart baseChart = getBaseChart();
-			Rectangle plotAreaBounds = baseChart.getPlotArea().getBounds();
-			ISeries[] series = baseChart.getSeriesSet().getSeries();
-			//
-			for(ISeries scatterSeries : series) {
-				if(scatterSeries != null) {
-					//
-					double[] xSeries = scatterSeries.getXSeries();
-					double[] ySeries = scatterSeries.getYSeries();
-					int size = scatterSeries.getXSeries().length;
-					String id = scatterSeries.getId();
-					//
-					for(int i = 0; i < size; i++) {
-						Point point = scatterSeries.getPixelCoordinates(i);
-						if(isPointVisible(point, plotAreaBounds)) {
-							System.out.println("\t" + xSeries[i] + "\t" + ySeries[i] + "\t" + point + "\t" + id);
-							baseChart.selectSeries(id);
-						}
-					}
-				}
-			}
-			//
-			System.out.println("\tSeries Selected:");
-			for(String selectedSeriesId : baseChart.getSelectedSeriesIds()) {
-				System.out.println("\t\t" + selectedSeriesId);
-			}
-			//
-			baseChart.redraw();
-			isCustomSelection = false;
-		}
-	}
-
-	private boolean isPointVisible(Point point, Rectangle plotAreaBounds) {
-
-		if(point.x >= 0 && point.x <= plotAreaBounds.width && point.y >= 0 && point.y <= plotAreaBounds.height) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 	private void applySettings(IScatterSeriesSettings scatterSeriesSettings, double x, double y, int symbolSize) {
